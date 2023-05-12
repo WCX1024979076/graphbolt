4482a77d55226a72669d5043de5966120cb0a1c6
WCX1024979076 <1024979076@qq.com>
2023-05-12 21:06:06 +0800

    增加迭代批次分析

--- tools/analysis/analysis.C
+++ tools/analysis/analysis.C
@@ -5,6 +5,7 @@
 #include <cassert>
 #include <ctime>
 #define ITER_NUM 50
+#define BATCH_TIME 10
 char name[100];
 
 double *read_data()
@@ -40,7 +41,7 @@ int parallel_main(int argc, char *argv[])
 {
     FILE *fp = freopen(NOTES_FILE, "r", stdin);
     
-    double batch_rate, degree_avg, base_graph_rate, batch_add_rate;
+    double batch_rate, degree_avg[5], base_graph_rate, batch_add_rate;
     long long batch_size, batch_time, snap_edge_num, snap_vertex_num;
     scanf("BATCH_SIZE = %lld\n", &batch_size);
     scanf("SNAP_VERTEX_NUM = %lld\n", &snap_vertex_num);
@@ -50,41 +51,8 @@ int parallel_main(int argc, char *argv[])
     scanf("batch_rate = %lf\n", &batch_rate);
     scanf("batch_size = %lld\n", &batch_size);
     scanf("batch_time = %lld\n", &batch_time);
-    scanf("degree_avg = %lf\n", &degree_avg);
-    
-    double *graphbolt_data[3], *tegra_data[3], *trad_data[3]; 
-    double *initial_data[9];
-    for (int i = 0; i < 3; i++)
-    {
-        read_name();
-        initial_data[i * 3] = read_data();
-        read_name();
-        graphbolt_data[i] = read_data();
 
-        read_name();
-        initial_data[i * 3 + 1] = read_data();
-        read_name();
-        tegra_data[i] = read_data();
-
-        read_name();
-        initial_data[i * 3 + 2] = read_data();
-        read_name();
-        trad_data[i] = read_data();
-    }
-    double *graphbolt_data_avg = get_avg(graphbolt_data);
-    double *tegra_data_avg = get_avg(tegra_data);
-    double *trad_data_avg = get_avg(trad_data);
-    int n1 = -1, n2 = -1; // TODO: 修正评估算法
-    for(int i = 0; i < ITER_NUM; i++)
-    {
-        if(trad_data_avg[i] < graphbolt_data_avg[i] && n1 == -1)
-            n1 = i;
-        if(tegra_data_avg[i] < graphbolt_data_avg[i] && n2 == -1)
-            n2 = i;
-    }
-    std::cout << "n1 = " << n1 << "; n2 = " << n2 << std::endl;
-    
-    FILE *fp1 = freopen("/home/wangcx/tmp/notes3.txt", "a", stdout);
+    FILE *fp1 = freopen("/home/wangcx/tmp/notes5.txt", "a", stdout);
     printf("BATCH_SIZE = %lld\n", batch_size);
     printf("SNAP_VERTEX_NUM = %lld\n", snap_vertex_num);
     printf("SNAP_EDGE_NUM = %lld\n", snap_edge_num);
@@ -93,30 +61,84 @@ int parallel_main(int argc, char *argv[])
     printf("batch_rate = %f\n", batch_rate);
     printf("batch_size = %lld\n", batch_size);
     printf("batch_time = %lld\n", batch_time);
-    printf("degree_avg = %f\n", degree_avg);
-    printf("n1 = %d, n2 = %d\n", n1, n2);
-    for(int i = 0; i < ITER_NUM; i++)
-        printf("%f ", graphbolt_data_avg[i]);
-    printf("\n");
-    for(int i = 0; i < ITER_NUM; i++)
-        printf("%f ", tegra_data_avg[i]);
-    printf("\n");
-    for(int i = 0; i < ITER_NUM; i++)
-        printf("%f ", trad_data_avg[i]);
-    printf("\n");
-    fclose(fp1);
-
-    for (int i = 0; i < 9; i++)
-        free(initial_data[i]);
+    for(int j = 0; j < batch_time; j++) {
+        scanf("degree_avg = %lf\n", &degree_avg[j]);
+    }
+    double *graphbolt_data[BATCH_TIME][3], *tegra_data[BATCH_TIME][3], *trad_data[BATCH_TIME][3]; 
+    double *graphbolt_data_avg[BATCH_TIME], *tegra_data_avg[BATCH_TIME], *trad_data_avg[BATCH_TIME]; 
+    double *initial_data[9];
     for (int i = 0; i < 3; i++)
     {
-        free(graphbolt_data[i]);
-        free(tegra_data[i]);
-        free(trad_data[i]);
+        read_name();
+        initial_data[i * 3] = read_data();
+        for(int j = 0; j < batch_time; j++)
+        {
+            read_name();
+            graphbolt_data[j][i] = read_data();
+        }
+
+        read_name();
+        initial_data[i * 3 + 1] = read_data();
+        for(int j = 0; j < batch_time; j++)
+        {
+            read_name();
+            tegra_data[j][i] = read_data();
+        }
+
+        read_name();
+        initial_data[i * 3 + 2] = read_data();
+        for(int j = 0; j < batch_time; j++)
+        {
+            read_name();
+            trad_data[j][i] = read_data();
+        }
     }
-    free(graphbolt_data_avg);
-    free(tegra_data_avg);
-    free(trad_data_avg);
-    fclose(fp);
+
+    for(int j = 0; j < batch_time; j++)
+    {
+        graphbolt_data_avg[j] = get_avg(graphbolt_data[j]);
+        tegra_data_avg[j] = get_avg(tegra_data[j]);
+        trad_data_avg[j] = get_avg(trad_data[j]);
+    } 
+    for(int j = 0; j < batch_time; j++)
+    {
+        int n1 = -1, n2 = -1; // TODO: 修正评估算法
+        for(int i = 0; i < ITER_NUM; i++)
+        {
+            if(trad_data_avg[j][i] < graphbolt_data_avg[j][i] && n1 == -1)
+                n1 = i;
+            if(tegra_data_avg[j][i] < graphbolt_data_avg[j][i] && n2 == -1)
+                n2 = i;
+        }
+        printf("degree_avg = %f\n", degree_avg[j]);
+        printf("n1 = %d, n2 = %d\n", n1, n2);
+    }
+    
+    for(int j = 0; j < batch_time; j++)
+    {
+        for(int i = 0; i < ITER_NUM; i++)
+            printf("%f ", graphbolt_data_avg[j][i]);
+        printf("\n");
+        for(int i = 0; i < ITER_NUM; i++)
+            printf("%f ", tegra_data_avg[j][i]);
+        printf("\n");
+        for(int i = 0; i < ITER_NUM; i++)
+            printf("%f ", trad_data_avg[j][i]);
+        printf("\n");
+    }
+    for (int i = 0; i < 9; i++)
+        free(initial_data[i]);
+    for(int j = 0; j < batch_time; j++)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            free(graphbolt_data[j][i]);
+            free(tegra_data[j][i]);
+            free(trad_data[j][i]);
+        }
+        free(graphbolt_data_avg[j]);
+        free(tegra_data_avg[j]);
+        free(trad_data_avg[j]);
+    }
     return 0;
 }
